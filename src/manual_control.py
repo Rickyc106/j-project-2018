5fb0a0b3c3c99a5b5e2497dccb9704409cd2ff6a
rickyc106 <rickyc106@gmail.com>
2018-11-02 12:47:05 -0900

    Add base motor control - untested

--- src/manual_control.py
+++ src/manual_control.py
@@ -4,28 +4,54 @@
 import time
 import RPi.GPIO as GPIO
 
-# Declare the GPIO settings
-GPIO.setmode(GPIO.BOARD)
-if (GPIO.getmode() == None):
-	print "Warning: GPIO Mode not set properly"
+# Define global objects
+global l_pwm, r_pwm
 
-'''
-# set up GPIO pins
-GPIO.setup(7, GPIO.OUT) # Connected to PWMA
-GPIO.setup(11, GPIO.OUT) # Connected to AIN2
-GPIO.setup(12, GPIO.OUT) # Connected to AIN1
-GPIO.setup(13, GPIO.OUT) # Connected to STBY
+def initialize(l_pin, r_pin):
+        # Declare the GPIO settings
+        GPIO.setmode(GPIO.BOARD)
+        if (GPIO.getmode() == None):
+                print "Warning: GPIO Mode not set"
+        else:
+                print "GPIO Mode set to: ", GPIO.getmode()
 
-# Drive the motor clockwise
-GPIO.output(12, GPIO.HIGH) # Set AIN1
-GPIO.output(11, GPIO.LOW) # Set AIN2
+        # set up GPIO pins
+        GPIO.setup(l_pin, GPIO.OUT) # Connected to left motor PWM
+        GPIO.setup(r_pin, GPIO.OUT) # Connected to right motor PWM
 
-# Set the motor speed
-GPIO.output(7, GPIO.HIGH) # Set PWMA
+        # Create PWM objects
+        l_pwm = GPIO.PWM(l_pin, 1000) # Pin number, Frequency
+        r_pwm = GPIO.PWM(r_pin, 1000)
 
-# Disable STBY (standby)
-GPIO.output(13, GPIO.HIGH)
+        # Set the motor speed
+        l_pwm.start(0) # Start PWM at 0%
+        r_pwm.start(0)
 
-# Wait 5 seconds
-time.sleep(5)
-'''
+# Stops both motors
+def motor_stop():
+        l_pwm.stop()
+        r_pwm.stop()
+
+# Turns forwards then turns backwards. Repeats until process dies.
+def control():
+        while 1:
+                for x in range(100):
+                        l_pwm.ChangeDutyCycle(x)
+                        r_pwm.ChangeDutyCycle(x)
+
+                        sleep(0.01)
+                        
+                for x in range(100, 0, -1):
+                        l_pwm.ChangeDutyCycle(x)
+                        r_pwm.ChangeDutyCycle(x)
+
+                        sleep(0.01)
+
+if __name__ == "__main__":
+        try:
+                initialize(12, 13)
+                control()
+
+        except KeyboardInterrupt:
+                motor_stop()   # Stops motors on Keyboard Interrupt
+                GPIO.cleanup() # Makes all outputs LOW
